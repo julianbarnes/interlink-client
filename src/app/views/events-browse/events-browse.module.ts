66d9211bac4bf86b52e17b9069de11e0cc41d447
Julian Barnes <julian.barnes100@gmail.com>
2021-01-24 20:04:42 -0500

    Adds the button menu in the event browse screen

--- src/app/views/events-browse/events-browse.module.ts
+++ src/app/views/events-browse/events-browse.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { EventsBrowseComponent } from './events-browse.component';
 import {ScrollingModule} from '@angular/cdk/scrolling';
-import { MatFormField, MatFormFieldModule, MatSelect, MatSelectModule } from '@angular/material';
+import { MatButtonModule, MatFormField, MatFormFieldModule, MatIconModule, MatSelect, MatSelectModule } from '@angular/material';
 import { ReactiveFormsModule } from '@angular/forms';
 import { EventDetailComponent } from '../event-detail/event-detail.component';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
 
 
 @NgModule({
@@ -16,7 +17,10 @@ import { EventDetailComponent } from '../event-detail/event-detail.component';
     ScrollingModule,
     MatSelectModule,
     MatFormFieldModule,
-    ReactiveFormsModule
+    MatButtonToggleModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatButtonModule
   ],
   exports: [MatCardModule]
 })
