3e047ac596df5f301745cab42d3af5d6f9664d11
Julian <julian.barnes100@gmail.com>
2022-09-14 21:45:16 -0400

    Adds print statement to server script

--- server.js
+++ server.js
@@ -13,4 +13,5 @@ res.sendFile(path.join(__dirname+'/dist/interlink-client/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
+console.log("Listening on port " + process.env.PORT || 8080)
 app.listen(process.env.PORT || 8080);
